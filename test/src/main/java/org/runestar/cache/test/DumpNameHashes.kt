61d6f79819e152f477b5aa16038c30fe5f51b625
hunterwb <hunter@hunterwb.com>
2018-09-01 18:34:45 -0700

    Remove name hash files

--- test/src/main/java/org/runestar/cache/test/DumpNameHashes.kt
+++ test/src/main/java/org/runestar/cache/test/DumpNameHashes.kt
@@ -19,17 +19,17 @@ fun main(args: Array<String>) {
             for (a in ref.archiveIds) {
                 val archive = checkNotNull(ref.archives[a])
                 val hash = checkNotNull(archive.nameHash)
-                s.appendln("$i,$a,-1,$hash")
+                s.appendln("$i\t$a\t-1\t$hash")
                 for (r in archive.recordIds) {
                     val record = checkNotNull(archive.records[r])
                     val hash2 = checkNotNull(record.nameHash)
                     if (hash2 != 0) {
-                        s.appendln("$i,$a,$r,$hash2")
+                        s.appendln("$i\t$a\t$r\t$hash2")
                     }
                 }
             }
         }
 
-        Files.write(Paths.get("names.csv"), s.toString().toByteArray(), StandardOpenOption.CREATE)
+        Files.write(Paths.get("name-hashes.tsv"), s.toString().toByteArray(), StandardOpenOption.CREATE)
     }
 }
