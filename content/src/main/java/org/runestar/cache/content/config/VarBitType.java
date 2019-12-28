84b47f54269cebd076ca01855f7a1f6270cd03df
Adam Corbin <acorbin3@gmail.com>
2019-12-28 05:35:55 -0500

    Adding a value to the VarBitType and conversion function to calculate the value

--- content/src/main/java/org/runestar/cache/content/config/VarBitType.java
+++ content/src/main/java/org/runestar/cache/content/config/VarBitType.java
@@ -12,7 +12,25 @@ public final class VarBitType extends ConfigType {
 
     public int endBit = 0;
 
-    @Override protected void decode0(Input in) {
+    public int value = 0;
+
+    /**
+     * The objective of this function is to calculate the varbit value based on the bit information given.
+     * First the mask is created, then anded with the baseValue then right shifted based on the start bit.
+     */
+    int getValue(int baseValue, int start, int end) {
+        int mask = 0;
+        for (int i = start; i < end + 1; i++) {
+            var val = 1 << i;
+            mask = mask | val;
+        }
+
+        return (baseValue & mask) >> start;
+    }
+
+
+    @Override
+    protected void decode0(Input in) {
         while (true) {
             int code = in.g1();
             switch (code) {
@@ -22,6 +40,7 @@ public final class VarBitType extends ConfigType {
                     baseVar = in.g2();
                     startBit = in.g1();
                     endBit = in.g1();
+                    value = getValue(baseVar, startBit, endBit);
                     break;
                 default:
                     unrecognisedCode(code);
